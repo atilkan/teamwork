51daf64ba7120ed034954e81566c899c20d9d522
atilkan <emrahatilkan@gmail.com>
2020-10-20 07:12:50 +0300

    remove planet module

--- src/store/modules/planets/mutation-types.ts
+++
@@ -1,7 +0,0 @@
-// We are manually namespacing types because otherwise we have to use createNamespacedHelpers and that is not flexible and cumbersome
-
-export default {
-  FETCH_PLANETS: "planets/FETCH_PLANETS",
-  GET_PLANETS: "planets/GET_PLANETS",
-  SET_PLANETS: "planets/SET_PLANETS",
-}
